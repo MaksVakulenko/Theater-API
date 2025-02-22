0f7685c0629575a0c0bc0bb22078308c905ab032
Maks Vakulenko <maksimvakulenkooo@gmail.com>
2025-02-22 19:50:36 +0100

    Set up Docker, Docker Compose, Docker Compose Local, and switched to PostgreSQL DB

--- theater_api/urls.py
+++ theater_api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/theatre/", include("theatre.urls", namespace="cinema")),
+    path("api/theatre/", include("theatre.urls", namespace="theatre")),
 ]
